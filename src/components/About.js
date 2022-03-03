1596ddcdeb7feef94c91ecbc6f539b7a3787938a
Indumati Raghumajji <indumati.raghumajji@o9solutions.com>
2022-03-03 17:36:56 +0530

    update

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import React from 'react';
 const About = () => (
   <div>
     <h2>About</h2>
-    This example shows how to use React Router!
+    This example shows how to use React Router! changed
   </div>
 );
 
